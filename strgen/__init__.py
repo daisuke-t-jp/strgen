23c655632b5eed6bb46e03d965a70140e2dbabd2
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-07-09 08:45:00 +0900

    Update

--- strgen/__init__.py
+++ strgen/__init__.py
@@ -1,10 +1,1 @@
-from .strgen import *
-
-__copyright__  = 'Copyright (C) 2020 Daisuke TONOSAKI'
 __version__ = '1.0.2'
-__license__ = 'MIT'
-__author__ = 'Daisuke TONOSAKI'
-__author_email__ = 'daisuke.t.jp@gmail.com'
-__url__ = 'https://github.com/daisuke-t-jp/strgen'
-
-__all__ = ['strgen']
